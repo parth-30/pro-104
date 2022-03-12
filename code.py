faa5f78c2f977faf32a2d020a8bc79f434b0648c
parth-30 <74844729+parth-30@users.noreply.github.com>
2022-03-12 17:59:44 -0600

    Add files via upload

---
+++ code.py
@@ -0,0 +1,25 @@
+import csv 
+
+with open("SOCR-HeightWeight.csv",newline='') as f:
+    reader= csv.reader(f)
+    file_data=list(reader)
+
+
+file_data.pop(0) 
+new_data=[] 
+
+#get height from file_data
+for i in range(len(file_data)):
+    n_num=file_data[i][1]
+    new_data.append(float(n_num)) 
+
+#mean =total/n
+n=len(new_data)
+total=0
+
+for x in new_data:
+    total=total+x
+
+mean=total/n
+
+print("Mean of Height :"+ str(mean))
